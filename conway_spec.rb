f13c788625db3903d11351bd7e1357e4c01f6466
Dan Dorman <dan.dorman@gmail.com>
2013-12-14 14:59:19 -0700

    asd;klfjals;dfjdslf

--- conway_spec.rb
+++ conway_spec.rb
@@ -1,6 +1,12 @@
 require "rspec"
 
 class LivingCell
+  attr_accessor :board
+
+  def initialize(board)
+    @board = board
+  end
+
   def next
     DeadCell.new
   end
@@ -10,10 +16,19 @@ class DeadCell
 end
 
 describe LivingCell do
+  let(:board) { double(:board) }
+
   describe "#next" do
     context "with 0 neighbors" do
       it "returns a dead cell" do
-        cell = LivingCell.new
+        cell = LivingCell.new(board)
+        expect(cell.next).to be_a DeadCell
+      end
+    end
+
+    context "with 1 neighbors" do
+      it "returns a dead cell" do
+        cell = LivingCell.new(board)
         expect(cell.next).to be_a DeadCell
       end
     end
